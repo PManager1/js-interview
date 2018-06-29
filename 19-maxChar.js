d4a9aee0855b73f5d227a8885f4d135eb5dd966d
jpca999 <jpca999@gmail.com>
2018-06-29 10:39:22 -0700

    now finding the maxChar also

--- 19-maxChar.js
+++ 19-maxChar.js
@@ -2,8 +2,10 @@
 
 function findMaxChars(string) {
   const	charMap = {};
+	let max = 0;
+	let maxChar = '';
 
-		for( let char of string ){
+		for( let char of string ){  // for string / array we use of
 			if ( charMap[char] ) {
 				charMap[char] ++;
 			}
@@ -14,6 +16,21 @@ function findMaxChars(string) {
 
 		console.log('22-  charMap = ', charMap );
 		// give the character that is max times repeated ?
+
+		for( let char in charMap ){  // for obj we use in
+			if ( charMap[char] > max ) {
+				 max = charMap[char];
+				 console.log(' 22 - max = ' ,  max );
+				 maxChar = char;
+				 console.log(' 25 - maxChar = ' ,  maxChar );
+			}
+			else {
+				charMap[char] = 1;
+			}
+		}
+
+
+
 }
 
 findMaxChars('Hello There!');
