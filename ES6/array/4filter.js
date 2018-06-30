fc277ddd9f8ced4e7c8443aec0a6fc793b5c9dc2
jpca999 <jpca999@gmail.com>
2018-06-29 19:03:31 -0700

    -

--- ES6/array/4filter.js
+++ ES6/array/4filter.js
@@ -1,15 +1,26 @@
-// Do try it witout looking. 
+// Do try it witout looking.
 
 let products = [
-  { name: 'cucumber', type: 'vegetable' },
-  { name: 'cucumber', type: 'fruit' },
-  { name: 'cucumber', type: 'fruit' },
-  { name: 'cucumber', type: 'vegetable' },
+  { name: 'cucumber', type: 'vegetable', quantity: 5, price: 1 },
+  { name: 'cucumber2', type: 'fruit', quantity: 15, price: 14 },
+  { name: 'cucumber3', type: 'fruit', quantity: 0, price: 6 },
+  { name: 'cucumber4`', type: 'vegetable', quantity: 4, price: 7 }
 ];
 
 
-let output = products.filter(function (p) {
-  return p.type ==='fruit'
-});
+// let output = products.filter(function (p) {
+//   return p.type ==='fruit'
+// });
+// console.log('12-  output = ', output);
 
-console.log('12-  output = ', output);
+// type vegetabels ,  quantity > 0, price less than 10
+
+
+let output = products.filter(function (p) {
+  return  p.type === 'vegetable'
+  && p.quantity > 0
+  &&  p.price < 10
+})
+
+console.log('22-  output = ', output);
+// type
