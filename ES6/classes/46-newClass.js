5dd26897282e1eb9fbe40bcb2035a250939d3f4e
jpca999 <jpca999@gmail.com>
2018-06-30 18:55:05 -0700

    now class working on 46- new Class

--- ES6/classes/46-newClass.js
+++ ES6/classes/46-newClass.js
@@ -3,11 +3,29 @@ class Car {
     console.log(' callign constructor ');
     this.title = options.title;
   }
+
   drive() {
     return 'driving sir';
   }
 }
 
-const car = new Car ({ title : "Bentley" });
-console.log(' car.title = ', car.title );
-console.log(' car = ', car.drive() );
+class Toyota extends Car {
+  constructor ( options ) {
+    super(options);   // Car.construtor.
+    this.color = options.color;
+  }
+
+  honk(){
+    return 'Toyota Honking';
+  }
+
+}
+
+
+// const car = new Car ({ title : "Bentl`ey" });
+// console.log(' car.title = ', car.title );
+// console.log(' car = ', car.drive() );`
+
+const toyota = new Toyota ({ color : "red", title: 'the title ' })
+console.log('29 -  toyota.honk() = ', toyota.honk() );
+console.log('30 -  toyota = ', toyota );
